467906ccd3ad405de5b38a684ac16895f145ecfa
benedikt <benedikt@pop-os.localdomain>
2022-10-20 17:54:00 +0200

    Probleme mit INTF

--- Statistic-Collector/CohesityStatsCollector.py
+++ Statistic-Collector/CohesityStatsCollector.py
@@ -38,12 +38,12 @@ unix_timestamp_now = int64(datetime.datetime.timestamp(date)*1000)
 unix_timestamp_start = (unix_timestamp_now - (ts_days * 24 * 60 *60 * 1000))
 print('Datetime: ',date, ' Unit Timestamp: ', unix_timestamp_now, ' - ', unix_timestamp_start)
 
-
 ts = []
 cpu = []
 nodeids = []
-nodectr = 0
 nodectrl = []
+
+nodectr = 0
 
 for node in nodelist:
     nodeid = node.attribute_vec[0].value.data.int_64_value
@@ -51,23 +51,65 @@ for node in nodelist:
 
     ## CPU
     cpu_timeseries = stats_controller.get_time_series_stats('kSentryNodeStats', 'kCpuUsagePct', unix_timestamp_start, entity_id=nodeid, entity_id_list=None, end_time_msecs=None, rollup_function=None, rollup_interval_secs=None)
-    cpu_timeseries_data = cpu_timeseries.data_point_vec
     nodectr += 1
-    print('Processing Data from Node: ', nodeid)
+    print('Processing CPU Data from Node: ', nodeid)
     for metric in cpu_timeseries.data_point_vec:
         ts.append(metric.timestamp_msecs*1000) 
         cpu.append(metric.data.double_value) 
         nodeids.append(nodeid)
         nodectrl.append(nodectr)
 
+
+
     
     temp_dict = {'time' : ts, 'nodectr' : nodectrl, 'nodeid' : nodeids, 'cpu' :cpu}
-    df = pd.DataFrame(temp_dict)
-    df.to_csv('cohesity.csv')
+df = pd.DataFrame(temp_dict)
+df.to_csv('cohesity_cpu.csv')
 
 
-df.sort_values(by='time', inplace=True)
-print(df)
+ts = []
+interfacename = []
+nodectrl = []
+rx_raw = []
+rxbs = []
+
+
+##Interface Stats
+interfaces = []
+interface_list = stats_controller.get_entities('kInterfaceStats', include_aggr_metric_sources=None, metric_names=None, max_entities=None)
+for interface in interface_list:
+    ifn = interface.entity_id.entity_id.data.string_value
+    if str(ifn).find('bond') != -1:
+        interfaces.append(ifn)
+
+for interface in interfaces:
+    ts = []
+    interfacename = []
+    rxbs = []
+    print('Processing Network RX Data from Node: ', interface)
+    timeseries = stats_controller.get_time_series_stats('kInterfaceStats', 'kRxBytes', unix_timestamp_start, entity_id=interface, entity_id_list=None, end_time_msecs=None, rollup_function=None, rollup_interval_secs=None)
+    for metric in timeseries.data_point_vec:
+        ts.append(metric.timestamp_msecs*1000) 
+        rx_raw.append(metric.data.double_value) 
+        interfacename.append(interface)
+        nodectrl.append(nodectr)
+
+    for i in range(len(rx_raw)):
+        if i == 0:
+            p = 1
+        else:
+            p = i
+        
+        rx_delta = rx_raw[p] - rx_raw[p-1]
+        rxbs.append(rx_delta)
+    
+    print(len(rxbs))
+    print(len(rx_raw))
+
+
+temp_dict = {'time' : ts, 'interface' : interfacename, 'rx' :rxbs}
+df = pd.DataFrame(temp_dict)
+df.to_csv('cohesity_intf.csv')
 
 
 
